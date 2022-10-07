baddf9a80cabdac96fb6a0e115bc81d0505958f8
Mateusz Szymiec <mattszymiec@gmail.com>
2022-10-07 13:11:42 +0200

    data analysis: Add deleting data and renaming headers functionalities

--- data_analysis/format_data.py
+++ data_analysis/format_data.py
@@ -1,6 +1,7 @@
 import glob
 import pandas as pd
 
+# load data
 path = ".\\data"
 files = glob.glob(path + "\\*.csv")
 
@@ -10,6 +11,16 @@ for file in files:
     data = pd.read_csv(file)
     dfs.append(data)
 
+# merge data frames
 df = pd.concat(dfs, ignore_index=True)
+
+# delete data from data frame
+df = df.drop(df.iloc[:, 0:2], axis='columns')
+
+# rename headers
+df = df.rename(
+    columns={
+        "C": "A",
+        "D": "B"})
 
 print(df)
